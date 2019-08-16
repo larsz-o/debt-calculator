7c09b37fb7475deb43554aabac80665921f6cb29
Lars Mackenzie <larsmackenzie@gmail.com>
2019-08-15 20:44:59 -0400

    build was in gitignore idk why

---
+++ build/precache-manifest.6d779ac1a7a05164547e7171eb87dc75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a81f55e87180fd95a1574860403379a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a44f09f44c3979ecf53",
+    "url": "/static/css/main.e2c06438.chunk.css"
+  },
+  {
+    "revision": "7615a3ff16a713130789",
+    "url": "/static/js/2.f6bf3ed7.chunk.js"
+  },
+  {
+    "revision": "4a44f09f44c3979ecf53",
+    "url": "/static/js/main.f8980f1f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
